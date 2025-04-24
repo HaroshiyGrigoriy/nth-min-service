62b3cc0c7783c982b5dfd53a10e044f85bec1d4b
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-04-24 15:09:49 +0300

    init: spring project skeleton

---
+++ src/test/java/com/grishiya/nth_min_service/NthMinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grishiya.nth_min_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NthMinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
