35a6a8f314f8e7e21674ab8dbb9c8dba424825b2
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-04-24 17:48:50 +0300

    Fix: понижен Spring Boot до 3.2.5 — решена проблема с NoSuchMethodError в springdoc-openapi, Swagger снова работает

--- src/test/java/com/grishiya/nth_min_service/controller/MinControllerTest.java
+++ src/test/java/com/grishiya/nth_min_service/controller/MinControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.file.Path;
@@ -22,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MinControllerTest {
     @Autowired
     MockMvc mockMvc;
-    @MockitoBean
+    @MockBean
     NumberService service;
 
     Path path = Path.of("C:/dummy/file.xlsx");
